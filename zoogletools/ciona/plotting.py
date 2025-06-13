99aab6baf6b945a768cc6a43a659f7b12a14e4d2
Dennis Sun <dennis.a.sun@gmail.com>
2025-06-13 14:48:32 -0700

    update plotting functions

--- zoogletools/ciona/plotting.py
+++ zoogletools/ciona/plotting.py
@@ -469,7 +469,7 @@ def _append_scatter_trace(
     fig: go.Figure,
     umap_df: pd.DataFrame,
     color_values: list,
-    position: str,
+    position: str | None = None,
 ) -> go.Scattergl:
     trace = go.Scattergl(
         x=umap_df["UMAP_1"],
@@ -483,6 +483,10 @@ def _append_scatter_trace(
         showlegend=False,
     )
 
+    if position is None:
+        fig.add_trace(trace)
+        return
+
     row, col = _map_position_to_row_col(position)
     fig.add_trace(trace, row=row, col=col)
 
@@ -490,8 +494,11 @@ def _append_scatter_trace(
 def _annotate_scatter_trace(
     fig: go.Figure,
     text: str,
-    position: str,
+    position: str | None = None,
 ) -> go.Figure:
+    if position is None:
+        position = "top-left"
+
     xref = f"x{_map_position_to_domain(position)} domain"
     yref = f"y{_map_position_to_domain(position)} domain"
 
@@ -514,7 +521,7 @@ def _annotate_scatter_trace(
 def append_seurat_clusters_trace(
     fig: go.Figure,
     umap_df: pd.DataFrame,
-    position: str,
+    position: str | None = None,
     title: str = "Seurat cluster",
 ) -> go.Figure:
     clusters = [str(i) for i in sorted(umap_df["seurat_clusters"].astype(int).unique())]
@@ -538,11 +545,35 @@ def append_seurat_clusters_trace(
     )
 
 
+DEFAULT_COLORBAR_PARAMS = dict(
+    title=dict(
+        text="Expression",
+        font=dict(family=apc.style_defaults.DEFAULT_FONT_PLOTLY + "-SemiBold", size=14),
+    ),
+    outlinewidth=0,
+    thickness=15,
+    ypad=0,
+    len=0.45,
+    y=0.78,
+    x=1,
+    xref="paper",
+    yref="paper",
+    yanchor="middle",
+    tickfont=dict(
+        size=12,
+        family=apc.style_defaults.MONOSPACE_FONT_PLOTLY,
+    ),
+)
+
+
 def append_expression_trace(
     fig: go.Figure,
     umap_df: pd.DataFrame,
-    position: str,
+    position: str | None = None,
     title: str = "Expression",
+    expression_colormap: apc.Gradient = GRAY_GRADIENT,
+    null_expression_color: str = apc.gray,
+    expression_colorbar_params: dict = DEFAULT_COLORBAR_PARAMS,
 ) -> go.Figure:
     umap_zero_df = umap_df[umap_df["gene_expression"] == 0]
     umap_nonzero_df = umap_df[umap_df["gene_expression"] != 0]
@@ -550,7 +581,7 @@ def append_expression_trace(
     _append_scatter_trace(
         fig,
         umap_zero_df,
-        apc.gray,
+        null_expression_color,
         position,
     )
 
@@ -560,31 +591,17 @@ def append_expression_trace(
         mode="markers",
         marker=dict(
             color=umap_nonzero_df["gene_expression"],
-            colorscale=GRAY_GRADIENT.to_plotly_colorscale(),
-            colorbar=dict(
-                title=dict(
-                    text="Expression",
-                    font=dict(family=apc.style_defaults.DEFAULT_FONT_PLOTLY + "-SemiBold", size=14),
-                ),
-                outlinewidth=0,
-                thickness=15,
-                ypad=0,
-                len=0.45,
-                y=0.78,
-                x=1,
-                xref="paper",
-                yref="paper",
-                yanchor="middle",
-                tickfont=dict(
-                    size=12,
-                    family=apc.style_defaults.MONOSPACE_FONT_PLOTLY,
-                ),
-            ),
+            colorscale=expression_colormap.to_plotly_colorscale(),
+            colorbar=expression_colorbar_params,
         ),
         customdata=umap_nonzero_df[SCATTER_CUSTOMDATA],
         hovertemplate=SCATTER_HOVERTEMPLATE,
         showlegend=False,
     )
+
+    if position is None:
+        fig.add_trace(trace)
+        return
 
     row, col = _map_position_to_row_col(position)
     fig.add_trace(trace, row=row, col=col)
@@ -599,7 +616,7 @@ def append_expression_trace(
 def append_tissue_type_trace(
     fig: go.Figure,
     umap_df: pd.DataFrame,
-    position: str,
+    position: str | None = None,
     title: str = "Cluster tissue type",
 ) -> go.Figure:
     _append_scatter_trace(
@@ -618,7 +635,7 @@ def append_tissue_type_trace(
 def append_cell_tissue_type_trace(
     fig: go.Figure,
     umap_df: pd.DataFrame,
-    position: str,
+    position: str | None = None,
     title: str = "Cell tissue type",
 ) -> go.Figure:
     _append_scatter_trace(
@@ -697,6 +714,9 @@ def plot_expression_umap(
         "tissue_type",
         "cell_tissue_type",
     ),
+    expression_colormap: apc.Gradient = GRAY_GRADIENT,
+    null_expression_color: str = apc.gray,
+    expression_colorbar_params: dict = DEFAULT_COLORBAR_PARAMS,
     marker_size: int = 3,
     width: int = 850,
     height: int = 800,
@@ -763,7 +783,13 @@ def plot_expression_umap(
             )
         elif plot == "expression":
             append_expression_trace(
-                fig, umap_df, position, title=f"<i>{hgnc_gene_symbol}</i> ({ky_id})"
+                fig,
+                umap_df,
+                position,
+                title=f"<i>{hgnc_gene_symbol}</i> ({ky_id})",
+                null_expression_color=null_expression_color,
+                expression_colormap=expression_colormap,
+                expression_colorbar_params=expression_colorbar_params,
             )
         elif plot == "tissue_type":
             append_tissue_type_trace(fig, umap_df, position, title="Cluster tissue type")
